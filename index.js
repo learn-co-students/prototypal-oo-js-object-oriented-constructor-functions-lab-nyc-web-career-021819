2047dff70184aedcdc35b5bbab03a5164facc079
Jessy Hu <jesshuy12@gmail.com>
2019-04-14 18:50:13 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+function Scooter(year, color, model) {
+    this.year = year
+    this.color = color
+    this. model = model
+} //end of Scooter
+
+ function Driver(name, age, experience) {
+    this.name = name
+    this.age = age
+    this.experience = experience
+}// end of Driver
+
+ function PickupLocation(address, city) {
+    this.address = address
+    this.city = city
+} // end of PickupLocation
